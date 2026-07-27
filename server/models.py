87f9bb9d7b28d509ec7eece87eb7c5a5d5a8955c
Ted karani <ted.karani@student.moringaschool.com>
2026-07-27 11:37:07 +0300

    Add Workout model

--- server/models.py
+++ server/models.py
@@ -12,3 +12,14 @@ class Exercise(db.Model):
 
     def __repr__(self):
         return f'<Exercise {self.id}, {self.name}, {self.category}>'
+
+class Workout(db.Model):
+    __tablename__ = 'workouts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    duration_minutes = db.Column(db.Integer)
+    notes = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Workout {self.id}, {self.date}, {self.duration_minutes} min>'    
