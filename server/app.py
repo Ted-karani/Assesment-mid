e33f75b503b663551eb1aacf4cbb020e3e22e545
Ted karani <ted.karani@student.moringaschool.com>
2026-07-27 18:28:36 +0300

    making the  API endpoints

--- server/app.py
+++ server/app.py
@@ -11,7 +11,41 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-# Define Routes here
+@app.route('/workouts', methods=['GET'])
+def get_workouts():
+    pass
+
+@app.route('/workouts', methods=['POST'])
+def create_workout():
+    pass
+
+@app.route('/workouts/<int:id>', methods=['GET'])
+def get_workout(id):
+    pass
+
+@app.route('/workouts/<int:id>', methods=['DELETE'])
+def delete_workout(id):
+    pass
+
+@app.route('/exercises', methods=['GET'])
+def get_exercises():
+    pass
+
+@app.route('/exercises', methods=['POST'])
+def create_exercise():
+    pass
+
+@app.route('/exercises/<int:id>', methods=['GET'])
+def get_exercise(id):
+    pass
+
+@app.route('/exercises/<int:id>', methods=['DELETE'])
+def delete_exercise(id):
+    pass
+
+@app.route('/workouts/<int:workout_id>/exercises/<int:exercise_id>/workout_exercises', methods=['POST'])
+def add_exercise_to_workout(workout_id, exercise_id):
+    pass
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
