8d2fcd76fcb734e1b92087065ba39bbaeb377657
Ted karani <ted.karani@student.moringaschool.com>
2026-07-27 10:52:40 +0300

    Add Exercise model

--- server/models.py
+++ server/models.py
@@ -2,4 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
-# Define Models here
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    category = db.Column(db.String)
+    equipment_needed = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f'<Exercise {self.id}, {self.name}, {self.category}>'
